651d8cbbd2843becd526893084950f870edff852
Igromant <igruliman@gmail.com>
2021-04-09 11:04:32 +0300

    feat: add cssmin in serveSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 // Static server
 function bs() {
@@ -19,11 +22,21 @@ function bs() {
 function serveSass() {
     return src("./sass/*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
 
 
 exports.serve = bs;
+
+//autoPrefixer
+
+
+
 
 //CssMin
