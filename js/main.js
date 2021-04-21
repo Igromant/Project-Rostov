cef2ca1d9ac41b1d75866a33b4d79634575b04b3
Igromant <igruliman@gmail.com>
2021-04-21 04:50:23 +0300

    feat: add new button up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*
+document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
@@ -19,4 +20,43 @@ document.addEventListener('keydown', function(event) {
     modal.classList.remove('modal--visible');
     }
 });
+*/
 
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),   
+        closeBtn = $('.modal__close');
+        
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible')
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible')
+    });
+    $(document).keyup(function (closeKey) {
+        //27 = escape
+        if(closeKey.which == 27 && modal.hasClass('modal--visible')){
+            modal.toggleClass('modal--visible')
+        }
+    });
+    $(document).click(function (closeKey) {
+        if($(closeKey.target).is('.modal')){
+            modal.toggleClass('modal--visible')
+        }
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.button__upward').fadeIn();
+        } else {
+            $('.button__upward').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('.button__upward').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
